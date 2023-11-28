7d48364cbcda246350bfff70cb5ad31e47bc035a
Parth Patel <parth05211@gmail.com>
2023-11-27 22:18:35 -0500

    Added change text functionality

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hi");
+const text = document.querySelector(".last-part");
+const setText = () =>{
+    setTimeout(()=>{
+        text.textContent = "web development.";
+    },0);
+    setTimeout(()=>{
+        text.textContent = "learn new stuffs.";
+    },5000);
+    setTimeout(()=>{
+        text.textContent = "contribute to the tech community.";
+    },10000);
+}
+
+setText();
+setInterval(setText,15000)
