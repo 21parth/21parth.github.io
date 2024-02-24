5f024eeed876107c62562b1bc28ee9bbc873fc37
Parth Patel <parth05211@gmail.com>
2024-02-24 00:09:36 -0500

    Commit

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const text = document.querySelector(".last-part");
-const setText = () =>{
-    setTimeout(()=>{
-        text.textContent = "web development.";
-    },0);
-    setTimeout(()=>{
-        text.textContent = "learn new stuffs.";
-    },4000);
-}
+const setText = () => {
+  setTimeout(() => {
+    text.textContent = "web development.";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "learn new stuffs.";
+  }, 4000);
+};
 
 setText();
-setInterval(setText,8000)
+setInterval(setText, 8000);
 
+$("#myList a").on("click", function (e) {
+  e.preventDefault();
+  //console.log("hi");
+  $(this).tab("show");
+});
